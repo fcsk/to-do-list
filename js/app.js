1dd79b0d83975c5967f579a6c44facbbcbe409c5
Konrad Gaj <kfocusek@gmail.com>
2023-03-27 21:15:19 +0200

    highlighting a task that has been done

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 const AddSectionTaskButton = document.querySelector(".addnew-task");
 const inputTextTask = document.querySelector(".Add-task");
 const TaskSubmitButton = document.querySelector(".add-task-button");
-const checkBox = document.querySelector(".task-checkbox");
 const PlaceForTasks = document.querySelector(".task-form");
-const newTask = document.createElement("div");
 
 AddSectionTaskButton?.addEventListener("click", () => {
   inputTextTask.style.visibility = "visible";
@@ -11,21 +9,35 @@ AddSectionTaskButton?.addEventListener("click", () => {
 
 TaskSubmitButton?.addEventListener("click", () => {
   const TextOfTask = document.getElementById("task-text").value;
-  const newCheckbox = document.createElement("input");
-  const newLabel = document.createElement("label");
 
-  newLabel.classList.add(".task-checkbox");
-  newLabel.innerHTML = `${TextOfTask}`;
+  if (TextOfTask == "" || null) {
+  } else {
+    const newTask = document.createElement("div");
+    const newCheckbox = document.createElement("input");
+    const newLabel = document.createElement("label");
 
-  newCheckbox.type = "checkbox";
-  newCheckbox.classList.add("task-checkbox");
-  newCheckbox.value = `${TextOfTask}`;
-  newCheckbox.name = `${TextOfTask}`;
+    newLabel.classList.add("task-checkbox");
+    newLabel.innerHTML = `${TextOfTask}`;
 
-  newTask.classList.add("created-task");
+    newCheckbox.type = "checkbox";
+    newCheckbox.classList.add("task-checkbox");
+    newCheckbox.value = `${TextOfTask}`;
+    newCheckbox.name = `${TextOfTask}`;
 
-  newTask.appendChild(newCheckbox);
-  newTask.appendChild(newLabel);
-  PlaceForTasks.appendChild(newTask);
-  document.getElementById("task-text").value = "";
+    newTask.classList.add("created-task");
+
+    newTask.appendChild(newCheckbox);
+    newTask.appendChild(newLabel);
+    PlaceForTasks.appendChild(newTask);
+    document.getElementById("task-text").value = "";
+
+    newCheckbox.addEventListener("change", (event) => {
+      const label = event.target.parentElement;
+      if (event.target.checked) {
+        label.style.textDecoration = "line-through";
+      } else {
+        label.style.textDecoration = "none";
+      }
+    });
+  }
 });
