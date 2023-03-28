191939235a13ea8d7fcc40d3ad3b0e57568ea62a
Konrad Gaj <kfocusek@gmail.com>
2023-03-28 15:49:08 +0200

    added edit button and new function keypress(it is a test) for add new task

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const AddSectionTaskButton = document.querySelector(".addnew-task");
 const inputTextTask = document.querySelector(".Add-task");
 const TaskSubmitButton = document.querySelector(".add-task-button");
 const PlaceForTasks = document.querySelector(".task-form");
+const taskText = document.querySelector("#task-text");
 
 AddSectionTaskButton?.addEventListener("click", () => {
   inputTextTask.style.visibility = "visible";
@@ -14,16 +15,23 @@ TaskSubmitButton?.addEventListener("click", () => {
   } else {
     const newTask = document.createElement("div");
     const newCheckbox = document.createElement("input");
-    const newLabel = document.createElement("label");
+    const newInput = document.createElement("input");
     const deleteButton = document.createElement("button");
     const deleteIcon = document.createElement("i");
+    const editIcon = document.createElement("i");
+    const EditButton = document.createElement("button");
 
     deleteIcon.classList.add("icon-trash");
     deleteButton.classList.add("delete-task-button");
     deleteButton.appendChild(deleteIcon);
 
-    newLabel.classList.add("task-checkbox");
-    newLabel.innerHTML = `${TextOfTask}`;
+    editIcon.classList.add("icon-pencil-alt");
+    EditButton.classList.add("edit-task-button");
+    EditButton.appendChild(editIcon);
+
+    newInput.classList.add("created-input");
+    newInput.value = TextOfTask;
+    newInput.setAttribute("disabled", "disabled");
 
     newCheckbox.type = "checkbox";
     newCheckbox.classList.add("task-checkbox");
@@ -33,8 +41,10 @@ TaskSubmitButton?.addEventListener("click", () => {
     newTask.classList.add("created-task");
 
     newTask.appendChild(newCheckbox);
-    newTask.appendChild(newLabel);
+    newTask.appendChild(newInput);
+    newTask.appendChild(EditButton);
     newTask.appendChild(deleteButton);
+
     PlaceForTasks.appendChild(newTask);
     document.getElementById("task-text").value = "";
 
@@ -50,5 +60,83 @@ TaskSubmitButton?.addEventListener("click", () => {
     deleteButton.addEventListener("click", () => {
       newTask.remove();
     });
+
+    newInput.addEventListener("keypress", (event) => {
+      if (event.keyCode === 13) {
+        newInput.setAttribute("disabled", "disabled");
+      }
+    });
+
+    EditButton.addEventListener("click", () => {
+      newInput.removeAttribute("disabled");
+      newInput.focus();
+    });
+  }
+});
+
+taskText?.addEventListener("keypress", (event) => {
+  if (event.keyCode === 13) {
+    const TextOfTask = document.getElementById("task-text").value;
+    if (TextOfTask == "" || null) {
+    } else {
+      const newTask = document.createElement("div");
+      const newCheckbox = document.createElement("input");
+      const newInput = document.createElement("input");
+      const deleteButton = document.createElement("button");
+      const deleteIcon = document.createElement("i");
+      const editIcon = document.createElement("i");
+      const EditButton = document.createElement("button");
+
+      deleteIcon.classList.add("icon-trash");
+      deleteButton.classList.add("delete-task-button");
+      deleteButton.appendChild(deleteIcon);
+
+      editIcon.classList.add("icon-pencil-alt");
+      EditButton.classList.add("edit-task-button");
+      EditButton.appendChild(editIcon);
+
+      newInput.classList.add("created-input");
+      newInput.value = TextOfTask;
+      newInput.setAttribute("disabled", "disabled");
+
+      newCheckbox.type = "checkbox";
+      newCheckbox.classList.add("task-checkbox");
+      newCheckbox.value = `${TextOfTask}`;
+      newCheckbox.name = `${TextOfTask}`;
+
+      newTask.classList.add("created-task");
+
+      newTask.appendChild(newCheckbox);
+      newTask.appendChild(newInput);
+      newTask.appendChild(EditButton);
+      newTask.appendChild(deleteButton);
+
+      PlaceForTasks.appendChild(newTask);
+      document.getElementById("task-text").value = "";
+
+      newCheckbox.addEventListener("change", (event) => {
+        const label = event.target.parentElement;
+        if (event.target.checked) {
+          label.style.textDecoration = "line-through";
+        } else {
+          label.style.textDecoration = "none";
+        }
+      });
+
+      deleteButton.addEventListener("click", () => {
+        newTask.remove();
+      });
+
+      newInput.addEventListener("keypress", (event) => {
+        if (event.keyCode === 13) {
+          newInput.setAttribute("disabled", "disabled");
+        }
+      });
+
+      EditButton.addEventListener("click", () => {
+        newInput.removeAttribute("disabled");
+        newInput.focus();
+      });
+    }
   }
 });
