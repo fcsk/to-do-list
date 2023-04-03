a7f0f2b101d225b4a0d8ce255b891b928c15fa1a
Konrad Gaj <kfocusek@gmail.com>
2023-04-03 12:56:45 +0200

    displays data from localstorage

--- js/app.js
+++ js/app.js
@@ -104,3 +104,87 @@ TaskSubmitButton?.addEventListener("click", () => {
     });
   }
 });
+
+window.addEventListener("load", () => {
+  let tasks = [];
+  if (localStorage.getItem(localStorageKey)) {
+    tasks = JSON.parse(localStorage.getItem(localStorageKey));
+  }
+
+  tasks.forEach((task) => {
+    const newTask = document.createElement("div");
+    const newCheckbox = document.createElement("input");
+    const newInput = document.createElement("textarea");
+    const deleteButton = document.createElement("button");
+    const deleteIcon = document.createElement("i");
+    const editIcon = document.createElement("i");
+    const EditButton = document.createElement("button");
+
+    deleteIcon.classList.add("icon-trash");
+    deleteButton.classList.add("delete-task-button");
+    deleteButton.appendChild(deleteIcon);
+
+    editIcon.classList.add("icon-pencil-alt");
+    EditButton.classList.add("edit-task-button");
+    EditButton.appendChild(editIcon);
+
+    newInput.classList.add("created-input");
+    newInput.value = task;
+    newInput.setAttribute("disabled", "disabled");
+    newInput.style.resize = "none";
+
+    newCheckbox.type = "checkbox";
+    newCheckbox.classList.add("task-checkbox");
+    newCheckbox.value = `${task}`;
+    newCheckbox.name = `${task}`;
+
+    newTask.classList.add("created-task");
+
+    newTask.appendChild(newCheckbox);
+    newTask.appendChild(newInput);
+    newTask.appendChild(EditButton);
+    newTask.appendChild(deleteButton);
+
+    PlaceForTasks.appendChild(newTask);
+
+    newCheckbox.addEventListener("change", (event) => {
+      const label = event.target.parentElement;
+      if (event.target.checked) {
+        label.style.textDecoration = "line-through";
+      } else {
+        label.style.textDecoration = "none";
+      }
+    });
+
+    deleteButton.addEventListener("click", () => {
+      const taskToDelete = newInput.value;
+      let tasks = [];
+      if (localStorage.getItem(localStorageKey)) {
+        tasks = JSON.parse(localStorage.getItem(localStorageKey));
+      }
+      const filteredTasks = tasks.filter((task) => task !== taskToDelete);
+      localStorage.setItem(localStorageKey, JSON.stringify(filteredTasks));
+
+      newTask.remove();
+    });
+
+    newInput.addEventListener("keypress", (event) => {
+      if (event.keyCode === 13) {
+        newInput.setAttribute("disabled", "disabled");
+      }
+    });
+
+    EditButton.addEventListener("click", () => {
+      newInput.removeAttribute("disabled");
+      newInput.style.resize = "vertical";
+      newInput.focus();
+    });
+
+    newInput.addEventListener("blur", () => {
+      if (newInput === document.activeElement) {
+      } else {
+        newInput.style.resize = "none";
+      }
+    });
+  });
+});
